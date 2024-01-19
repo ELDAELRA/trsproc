16ce552c8131fbb3f8ecc739c5b982f5ceb08713
3elele <gabriele@elda.org>
2024-01-19 15:45:10 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="trsproc",
+    version="1.0.0",
+    author="Gabriele Chignoli",
+    author_email="gabriele@elda.org",
+    description="A Python library to process Transcriber TRS files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
+
