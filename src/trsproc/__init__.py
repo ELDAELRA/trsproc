5fd6ec129278546368a439942e3adc813d309da2
3elele <gabriele@elda.org>
2024-02-23 14:38:42 +0100

    Patch config and documentation

--- src/trsproc/__init__.py
+++ src/trsproc/__init__.py
@@ -1,12 +1,1 @@
-from importlib import resources
-try:
-    import tomllib
-except ModuleNotFoundError:
-    import tomli as tomllib
-
-# Read opts of trsproc from config file
-_cfg = tomllib.loads(resources.read_text("trsproc", "config.toml"))
-FLAGS = _cfg["flags"]
-CORRECTIONS = _cfg["corrections"]
-
 __version__ = "1.4.8"
--- src/trsproc/__main__.py
+++ src/trsproc/__main__.py
@@ -18,6 +18,31 @@ import trsproc
 from trsproc import parser
 
 console = Console()
+FLAGS = {
+    '-cne':("deletes the Named Entity annotations if any are present in the input TRS.", "Cleaning NE annotation from TRS in", "trs", "parser.TRSParser.cleanNEfromTRS"),
+    '-crt':("Correction de TRS selon problèmes rencontrées", "TRS custom correction in", "trs", "None"),
+    '-ne':("extracts the Named Entity annotations if any are present in the input TRS and put them in a tabular file.", "NE extraction from", "trs", "parser.TRSParser.retrieveNEToTsv"),
+    '-pne':("pre-annotates the input TRS using the table created in the `-ne` flag as a custom annotation dictionnary.", "NE pre-annotation for TRS in", "trs", "utils.trsPreannotation"),
+    '-prt':("print the parsed TRS contents directly in the console.", "Printing TRS contents", "trs", "parser.TRSParser.print"),
+    '-rpt':("performs the operations of the `-tmp` and `-vsi` flags in order to obtain the basic elements for data validation. An additional report is produced with pause segments longer than 0.5s and speech segments shorter than 10s.", "Creating validation report for target Section", "trs", "utils.tmpReport"),
+    '-rs':("calculates the minimum sample needed for the validation of the input TRS transcription and the extracts random segments (audio and text, the latter in a tabular file) according to a given quantity.", "Extracting random segments from", "trs", "utils.randomSampling"),
+    '-rsne':("calculates the minimum sample needed for the validation of Named Entities of the input TRS and extracts them (audio segments and text, the latter in a tabular file) randomly by a given amount.", "Extracting random NE from", "trs", "utils.randomSamplingNE"),
+    '-tg':("converts TRS files to TextGrid files.", "Converting to TextGrid in", "trs", "parser.TRSParser.trsToTextGrid"),
+    '-tgrs':("converts TextGrid files to TRS files.", "Converting TextGrid to TRS in", "TextGrid", "parser.TRSParser.textGridToTRS"),
+    '-tmp':("creates TRS-temporary files in a directory named \"tmp\". By default, these files contain only the target section(s) of the original TRS.", "Writing temporary TRS in", "trs", "parser.TRSParser.trsTMP"),
+    '-trs':("rewrites a TRS file using the input txt file and a TRS-placeholder placed in a subfolder of the parent input folder. The rewritten TRS will have the content of the txt and the structure of the TRS-placeholder.", "Re-writing TRS in", "txt", "parser.TRSParser.txtToTrs"),
+    '-tsv':("produces a tabular file with the structures and contents of the TRS files.", "Writing tsv from TRS in", "trs", "parser.TRSParser.trsToTsv"),
+    '-txt':("creates txt and TRS-placeholder files. The first only containing the transcription of the original TRS, the latter having its XML structure.", "Extracting txt and TRS-placeholder in", "trs", "parser.TRSParser.trsToTxt"),
+    '-vad':("converts TextGrid files resulting from the use of a voice activity detection algorithm (VAD) into TRS files.", "Converting TextGrid-VAD in", "TextGrid", "parser.TRSParser.vadToTRS"),
+    '-vsi':("produces a tabular file containing basic lexical information and statistics concerning the input TRS.", "Extracting TRS statistics in", "trs", "parser.TRSParser.validateTRS")
+}
+
+CORRECTIONS = {
+    1:("turnDifferenceTRS", "search for differences in segmentation for the input TRS and its twin placed in a subfolder named \"twin\".", "utils.turnDifferenceTRS"),
+    2:("trsEmptySpaceBeforeNE", "adds an empty space before each NE annotation and save the new TRS in a separate subfolder", "utils.trsEmptySpaceBeforeNE"),
+    3:("correctionLà", "corrects sentences ending with là in la. This needs the execution of -txt flag beforehand.", "utils.correctionLà"),
+    4:("correctionMaj", "corrects misplaced capiral letters.", "utils.correctionMaj")
+}
 
 #----------
 def main():
@@ -46,17 +71,17 @@ def main():
         else:
             langT = 'eu'
 
-        procParam = trsproc.FLAGS[p]
+        procParam = FLAGS[p]
         docus = sorted(glob.glob(os.path.join(docPath, f'*.{procParam[2]}')))
         docus = list(set(docus))
         if p in ['-rs', '-rsne']:
             exec(f"{procParam[-1]}(docus, docPath)")
         else:
             if p == '-crt':
-                for c in trsproc.CORRECTIONS:
-                    print(f"{c} -> {trsproc.CORRECTIONS[c][0]}, {trsproc.CORRECTIONS[c][1]}")
+                for c in CORRECTIONS:
+                    print(f"{c} -> {CORRECTIONS[c][0]}, {CORRECTIONS[c][1]}")
                 funChoice = int(input("Insert the desired correction function number (list above)\t"))
-                fun = trsproc.CORRECTIONS[funChoice][-1]
+                fun = CORRECTIONS[funChoice][-1]
             with console.status(f"{procParam[1]} {docPath} with {len(docus)} files", spinner='point') as status:
                 for d in docus:
                     if procParam[2] == 'trs':
@@ -75,8 +100,8 @@ def main():
             console.print(status)
     except (IndexError, KeyError):
         print(f"Invalid flag, Please choose from the list below and call the program again:")
-        for p in trsproc.FLAGS:
-            print(f"{p} -> {trsproc.FLAGS[p][0]}")
+        for p in FLAGS:
+            print(f"{p} -> {FLAGS[p][0]}")
 
     print("--- %s sec taken ---" % round((time.time() - start_time), 2))
 
