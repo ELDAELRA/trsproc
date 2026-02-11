f22239858ed129bdae0fc84164958f333793145e
Florian PHILIPPE <florian.philippegm@gmail.com>
2026-02-11 17:33:28 +0100

    move txt_to_trs inside of TRSParser
    this function doesn't access any property or even parse any trs file, so it doesn't belong in the parser class

--- src/trsproc/parser.py
+++ src/trsproc/parser.py
@@ -41,6 +41,69 @@ def praat_snr_for_segment(audio, seg_start, seg_end):
     return round(mean_snr, 2)
 
 
+def txt_to_trs(input_txt, from_correction=0):
+    # TODO: remove the need for placeholders
+    # TODO: add an option to choose where to save the new .trs (or perhaps overwrite the original one ?)
+    """
+    >_ txt file having the transcription to be rewritten in a TRS, one segment per line and line length = last placeholder
+    >>> rewritten TRS, from_correction parameter helps to identify the file name
+    """
+    output_trs_correct = ""
+    txt_path, txt_name = os.path.split(input_txt)
+    txt_name = txt_name.split(".")[0]
+    txt_folder = os.path.basename(txt_path)
+    origin_path = txt_path.rstrip(txt_folder)
+    correction_level = {
+        0: "",
+        1: "_csp",
+        2: "_sp",
+        3: "_gram",
+        12: "_csp_sp",
+        13: "_csp_gram",
+        23: "_sp_gram",
+        123: "_csp_sp_gram",
+    }
+    ## find the correct text to rewrite
+    if from_correction != 0:
+        corrected_folder = os.path.join(txt_path, "corrections", "corrected")
+        os.makedirs(corrected_folder, exist_ok=True)
+        correction_folder = correction_level[from_correction].split("_")[-1]
+        txt_input = os.path.join(
+            txt_path,
+            "corrections",
+            correction_folder,
+            f"{txt_name}{correction_level[from_correction]}.txt",
+        )
+        trs_output = os.path.join(
+            corrected_folder, f"{txt_name}{correction_level[from_correction]}.trs"
+        )
+    else:
+        txt_input = os.path.join(txt_path, f"{txt_name}.txt")
+        trs_output = os.path.join(txt_path, f"{txt_name}.trs")
+    ## seach for the TRS placeholder
+    trs_placeholder = os.path.join(
+        origin_path, "placeholder", f"{txt_name}_placeholder.trs"
+    )
+    trs_input = open(trs_placeholder, encoding="utf-8").read()
+    trs_list = trs_input.split("\n")
+    txt_input = open(txt_input, encoding="utf-8").read()
+    txt_list = txt_input.split("\n")
+    print(f"\N{PACKAGE} Re-writing {txt_name}...")
+    for l in trs_list:
+        if len(l) == 0 or re.search("<.*>", l):
+            output_trs_correct += f"{l}\n"
+        ## writing TRS original XML structure into
+        elif re.search("[placeholder .+]", l):
+            plh = l.split(" ")[1]
+            plh = int(plh.rstrip("]"))
+            output_trs_correct += f"{txt_list[plh]}\n"
+        ## adding new txt content
+    with open(trs_output, "w", encoding="utf-8") as f_trs:
+        f_trs.write("".join(output_trs_correct))
+
+    return
+
+
 class TRSParser:
     def __init__(self, trs_in, audio_format="wav", lang="eu"):
         self.tree = ElementTree.parse(trs_in)
@@ -325,66 +388,6 @@ class TRSParser:
         file_output = os.path.join(path_txts, f"{self.filename}.txt")
         with open(file_output, "w", encoding="utf-8") as f_txt:
             f_txt.write("".join(output_txt))
-
-        return
-
-    def txt_to_trs(input_txt, from_correction=0):
-        """
-        >_ txt file having the transcription to be rewritten in a TRS, one segment per line and line length = last placeholder
-        >>> rewritten TRS, from_correction parameter helps to identify the file name
-        """
-        output_trs_correct = ""
-        txt_path, txt_name = os.path.split(input_txt)
-        txt_name = txt_name.split(".")[0]
-        txt_folder = os.path.basename(txt_path)
-        origin_path = txt_path.rstrip(txt_folder)
-        correction_level = {
-            0: "",
-            1: "_csp",
-            2: "_sp",
-            3: "_gram",
-            12: "_csp_sp",
-            13: "_csp_gram",
-            23: "_sp_gram",
-            123: "_csp_sp_gram",
-        }
-        ## find the correct text to rewrite
-        if from_correction != 0:
-            corrected_folder = os.path.join(txt_path, "corrections", "corrected")
-            os.makedirs(corrected_folder, exist_ok=True)
-            correction_folder = correction_level[from_correction].split("_")[-1]
-            txt_input = os.path.join(
-                txt_path,
-                "corrections",
-                correction_folder,
-                f"{txt_name}{correction_level[from_correction]}.txt",
-            )
-            trs_output = os.path.join(
-                corrected_folder, f"{txt_name}{correction_level[from_correction]}.trs"
-            )
-        else:
-            txt_input = os.path.join(txt_path, f"{txt_name}.txt")
-            trs_output = os.path.join(txt_path, f"{txt_name}.trs")
-        ## seach for the TRS placeholder
-        trs_placeholder = os.path.join(
-            origin_path, "placeholder", f"{txt_name}_placeholder.trs"
-        )
-        trs_input = open(trs_placeholder, encoding="utf-8").read()
-        trs_list = trs_input.split("\n")
-        txt_input = open(txt_input, encoding="utf-8").read()
-        txt_list = txt_input.split("\n")
-        print(f"\N{PACKAGE} Re-writing {txt_name}...")
-        for l in trs_list:
-            if len(l) == 0 or re.search("<.*>", l):
-                output_trs_correct += f"{l}\n"
-            ## writing TRS original XML structure into
-            elif re.search("[placeholder .+]", l):
-                plh = l.split(" ")[1]
-                plh = int(plh.rstrip("]"))
-                output_trs_correct += f"{txt_list[plh]}\n"
-            ## adding new txt content
-        with open(trs_output, "w", encoding="utf-8") as f_trs:
-            f_trs.write("".join(output_trs_correct))
 
         return
 
